bbd2ac1d316b7758eecbc25badb27c1cc33ac5b8
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-13 13:33:44 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+// import { Provider } from 'react-redux'
+// import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
-import store from './app/store'
+// import store from './app/store'
 
 // import 'antd/dist/antd.css'
 
