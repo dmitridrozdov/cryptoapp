f69740f77efd1d6d90ed7c633bfa5ea4fc091b67
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-13 13:33:41 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
     return (
         <div>
-            
+            <h1>Cryptoverse </h1>
         </div>
     )
 }
