673977c9aede0e8f0148771144f5db9a93f01f71
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-18 11:21:11 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import { Provider } from 'react-redux'
-// import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 // import store from './app/store'
@@ -19,4 +19,9 @@ import App from './App'
 //     </React.StrictMode>,
 //     document.getElementById('root'),
 //   )
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root')
+)
