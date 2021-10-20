72b4a0bc905322a44570119156af04e026562e14
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-20 13:11:14 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,8 +34,18 @@ const App = () => {
                 </Layout>
             </div>
             <div className="footer">
-
-            </div>
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+                    <Link to="/">
+                        Cryptoverse Inc.
+                    </Link> <br />
+                    All Rights Reserved.
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+        </div>
         </div>
     )
 }
