7cb9ec69dce77f13cc167ca6cc34f09b42eae66a
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-21 14:34:14 +1100

    Create cryptoApi.js

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': 'df6dbe4c2emsh96b91ea5b45846cp18dcbfjsn3f3462d59ca3'
+//     }
+//   };
