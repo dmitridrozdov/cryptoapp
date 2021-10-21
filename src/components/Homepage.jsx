bac0b5cdd480123973eddffba4e35a4b940c328f
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-21 14:12:00 +1100

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+
+        </>
     )
 }
 
