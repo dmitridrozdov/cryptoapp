fda06e0e3d629d3766031a27d51107bce79c659c
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-25 17:15:42 +1100

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,22 +1,55 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUrl = ''
+// const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges'
+
+// const cryptoApiHeaders = {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'df6dbe4c2emsh96b91ea5b45846cp18dcbfjsn3f3462d59ca3'
+// }
+
+// const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+// export const cryptoApi = createApi({
+//     reducerPath: 'cryptoApi',
+//     baseQuery: fetchBaseQuery({ baseUrl }),
+//     endpoints: (builder) => ({
+//         getCryptos: builder.query({
+//           query: (count) => createRequest(`/coins?limit=${count}`),
+//         }),
+//         getExchanges: builder.query({
+//             query: () => createRequest('/exchanges'),
+//           }),
+//     })
+// })
+
+// export const { useGetCryptosQuery, useGetExchangesQuery } = cryptoApi
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': '',
-    'x-rapidapi-key': ''
-}
+  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+};
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-          query: () => createRequest(`/coins`),
-        })
-    })
-})
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+    }),
+  }),
+});
 
-export const { useGetCryptosQuery } = cryptoApi
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
