fc3eee18fb44ab32ebfd906692036ea6317cf675
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-26 10:25:10 +1100

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
+import Cryptocurrencies from './Cryptocurrencies'
+import News from './News'
+import Loader from './Loader'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
@@ -28,6 +31,12 @@ const Homepage = () => {
                 <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
                 <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
             </div>
+            <Cryptocurrencies simplified />
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3}><Link to="/news">Show more</Link></Title>
+            </div>
+            <News simplified />
         </>
     )
 }
