a119cf5b1458e7e64029ffee80202b6ef1aeafad
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-25 11:13:38 +1100

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,20 @@
-// var options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
-//     headers: {
-//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//       'x-rapidapi-key': ''
-//     }
-//   };
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+const baseUrl = ''
 
 const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-host': '',
     'x-rapidapi-key': ''
-};
+}
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+          query: () => createRequest(`/exchanges`),
+        })
+    })
+})
