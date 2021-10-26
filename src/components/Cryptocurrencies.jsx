dcd24c5044be02cf1b9b611224971f465c71f34d
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-27 10:46:56 +1100

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import { Card, Row, Col, Input } from 'antd'
 
 const Cryptocurrencies = () => {
     return (
