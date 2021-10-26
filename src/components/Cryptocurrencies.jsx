0b4be2a2049d47957952a133884196af5ef54ad2
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-27 10:56:56 +1100

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
-    const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(10)
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+
+    console.log('cryptos')
+    console.log(cryptos)
+
     return (
         <div>
             Cryptocurrencies
