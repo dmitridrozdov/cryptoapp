c79489932dc458ffa987cd74d751717edd1ac00f
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-27 17:26:42 +1100

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -3,13 +3,13 @@ import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import Loader from './Loader'
 
 const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
-
-    console.log(cryptos)
+    if (isFetching) return <Loader />
 
     return (
         <>
