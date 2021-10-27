660d4c81446eb80508a2a0bca9944755a169b2c7
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-27 12:15:50 +1100

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -13,7 +13,13 @@ const Cryptocurrencies = () => {
     return (
         <>
             <Row gutter={[32, 32]} className="crypto-card-container">
-                
+                {cryptos?.map((currency) => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                        <Link key={currency.id} to={`/crypto/${currency.id}`}>
+                            
+                        </Link>
+                    </Col>
+                ))}
             </Row>
         </>
     )
