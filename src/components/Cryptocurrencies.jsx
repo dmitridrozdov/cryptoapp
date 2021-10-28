8b4d4eafefdbeb70bd927241aeb44b0d3f725543
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-28 11:08:53 +1100

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -9,10 +9,16 @@ const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+    const [searchTerm, setSearchTerm] = useState('')
     if (isFetching) return <Loader />
 
     return (
         <>
+            {!simplified && (
+                <div className="search-crypto">
+                <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value.toLowerCase())} />
+                </div>
+            )}
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
