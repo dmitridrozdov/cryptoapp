c51c63514575c9e37daf39c180f30ea63ed1441c
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-28 14:26:33 +1100

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
 import moment from 'moment'
+import { useGetCryptosQuery } from '../services/cryptoApi'
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import Loader from './Loader'
 
 const News = () => {
     return (
