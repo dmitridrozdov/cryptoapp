9c28e2f9aceaa255389dd429a7d4bef2149c58b4
dmitridrozdov <dm.drozdov@gmail.com>
2021-10-30 11:19:57 +1100

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -27,6 +27,14 @@ const News = ({ simplified }) => {
                                 <Title className="news-title" level={4}>{news.name}</Title>
                                 <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
                             </div>
+                            <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
+                            <div className="provider-container">
+                                <div>
+                                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                                    <Text className="provider-name">{news.provider[0]?.name}</Text>
+                                </div>
+                                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                            </div>
                         </a>
                     </Card>
                 </Col>
