91cfdaa693ddc7ea78e17ae8216eee930250bbd1
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-01 10:21:28 +1100

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import HTMLReactParser from 'html-react-parser'
+import { useParams } from 'react-router-dom'
+import millify from 'millify'
+import { Col, Row, Typography, Select } from 'antd'
 
 const CryptoDetails = () => {
     return (
