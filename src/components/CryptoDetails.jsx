5ccad67bdc06c60bd87140e8a35a88d91473264d
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-01 12:15:14 +1100

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -7,7 +7,7 @@ import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCi
 
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi'
 import Loader from './Loader'
-import LineChart from './LineChart'
+// import LineChart from './LineChart'
 
 const { Title, Text } = Typography
 const { Option } = Select
@@ -15,6 +15,25 @@ const { Option } = Select
 const CryptoDetails = () => {
     const { coinId } = useParams()
     const [timeperiod, setTimeperiod] = useState('7d')
+    const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
+
+    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y']
+
+    const stats = [
+        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+        { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+        { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+        { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+        { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+    ]
+
+    const genericStats = [
+        { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+        { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+        { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+        { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+    ]
 
     return (
         <div>
