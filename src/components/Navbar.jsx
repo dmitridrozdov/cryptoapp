f01942aac88b4b6f0fa618e4bb48eb2b6ffff30d
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-02 11:33:04 +1100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,13 @@ const Navbar = () => {
     const [activeMenu, setActiveMenu] = useState(true)
     const [screenSize, setScreenSize] = useState(undefined)
 
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth)
+        window.addEventListener('resize', handleResize)
+        handleResize()
+        return () => window.removeEventListener('resize', handleResize)
+      }, [])
+
     return (
         <div className="nav-container">
             <div className="logo-container">
