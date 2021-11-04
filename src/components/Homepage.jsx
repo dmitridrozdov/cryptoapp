aa4b1048759e3f2a773066e07e3889b85676ad8f
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-05 10:18:33 +1100

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import millify from 'millify'
-import { Typography, Row, Col, Statistic } from 'antd'
+import { Typography, Card, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 import Cryptocurrencies from './Cryptocurrencies'
 import News from './News'
@@ -18,13 +18,15 @@ const Homepage = () => {
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
-            <Row gutter={[32, 32]}>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-                <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
-                <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
-                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
-            </Row>
+            <Card  hoverable>
+                <Row gutter={[32, 32]}>
+                    <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+                    <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+                    <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+                    <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+                    <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+                </Row>
+            </Card>
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
                 <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
