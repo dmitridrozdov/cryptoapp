aeee36d3fdd3984499ec8a4d499e84d077549089
dmitridrozdov <dm.drozdov@gmail.com>
2021-11-05 16:01:04 +1100

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -15,6 +15,8 @@ const Homepage = () => {
     const globalStats = data?.data?.stats
     if (isFetching) return <Loader />
 
+    console.log(globalStats.total24hVolume)
+
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
@@ -23,7 +25,7 @@ const Homepage = () => {
                     <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
                     <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
                     <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
-                    <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+                    {/* <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col> */}
                     <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
                 </Row>
             </Card>
